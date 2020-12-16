0278f1e1f984f6acabe6ef2d4bf59de314f0aa9b
Willem Franco <willemfranco@gmail.com>
2020-12-16 13:24:50 -0500

    Search View

---
+++ src/views/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid my-3">
+    <Music v-bind="parsed" />
+  </div>
+</template>
+
+<script>
+import Music from '@/components/Music';
+
+export default {
+  name: 'Search',
+  components: { Music },
+  props: {
+    query: {type: String},
+    page: {type: Number},
+  },
+  computed: {
+    parsed() {
+      return {
+        page: parseInt(this.page) || 1,
+        query: String(this.query || ''),
+      };
+    },
+  },
+}
+</script>
