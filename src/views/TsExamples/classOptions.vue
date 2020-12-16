407a7ccd17e3d94f405029f2d6610e7a1c8b7eac
Willem Franco <willemfranco@gmail.com>
2020-12-16 13:30:05 -0500

    Component Examples in TypeScript

---
+++ src/views/TsExamples/classOptions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="example">
+    Typescript component, using class with Options decorator.
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+  data: () => ({
+  }),
+})
+export default class Home extends Vue {}
+</script>
---
+++ src/views/TsExamples/defineComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="example">
+    Typescript component, using defineComponent.
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+  },
+  data: () => ({
+  }),
+});
+</script>
