11085f823af428ce28a8c3441600fce0be3c1040
Willem Franco <willemfranco@gmail.com>
2020-12-16 13:24:36 -0500

    Home View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Container class="home d-flex flex-column" fluid>
+    <div class="flex-fill"></div>
+    <div class="text-center">
+      <h1>{{ t('spotify_demo') }}</h1>
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <h3>
+        Vue 3 + bootstrap
+      </h3>
+      <h5 class="text-muted">
+        {{ t('also_contains') }}:
+        jsx, typescript
+      </h5>
+      <h5 class="text-muted">{{ t('I18n') }} (i18n)</h5>
+      <h5 class="text-muted">{{ t('global_storage') }}</h5>
+      <h6>
+        <span className="mr-2">{{ t('source_code') }}:</span>
+        <a :href="source_code">{{ source_code }}</a>
+      </h6>
+    </div>
+    <div class="flex-fill"></div>
+  </Container>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script>
+import { useI18n } from 'vue-i18n';
+import Container from '../components/Container';
+import { source_code } from '../keys';
 
-@Options({
+export default {
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+    Container,
+  },
+  setup() {
+    const { t, d, locale } = useI18n();
+
+    return { t, d, locale, source_code };
+  },
+}
 </script>
+
+<style lang="scss" scoped>
+main {
+  background-color: #282c34;
+  color: white;
+}
+</style>
